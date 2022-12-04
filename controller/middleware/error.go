e19de62e4a93cc06cbbe7e97d9835c494befb1af
arai0724 <arai0724yutaarai@gmail.com>
2022-12-04 16:15:43 +0900

    when there is no errorToPrint.Menta, prevent it from panicing

--- controller/middleware/error.go
+++ controller/middleware/error.go
@@ -7,7 +7,7 @@ import (
 func HandleErrors(c *gin.Context) {
 	c.Next()
 	errorToPrint := c.Errors.ByType(gin.ErrorTypePublic).Last()
-	if errorToPrint != nil {
+	if errorToPrint != nil && errorToPrint.Meta != nil {
 		statusCode := errorToPrint.Meta.(int)
 		errMsg := errorToPrint.Error()
 		c.JSON(statusCode, gin.H{
