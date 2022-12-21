4de9dd3b8d7f39e7e0a571535c12cd6887164810
arai0724 <arai0724yutaarai@gmail.com>
2022-12-21 15:35:12 +0900

    fix the code as I noticed I should output error wrriten in Japanese by myself

--- model/todo_test.go
+++ model/todo_test.go
@@ -9,10 +9,16 @@ import (
 
 func TestTodoValidate(t *testing.T) {
 	// 正常系データ
+	t.Parallel()
+
 	testNormalTodoRequest := TodoRequest{Title: "goの勉強", Description: "テストコードを書く", Status: "始めたばかり", Priority: 1, UserID: 1}
-	assert.NoError(t, testNormalTodoRequest.TodoValidate(&gin.Context{}))
+	if ok := assert.NoError(t, testNormalTodoRequest.TodoValidate(&gin.Context{})); !ok {
+		t.Error("想定とは異なり、バリデーションエラーが発生しました。")
+	}
 
 	// 異常系データ(UserIDが存在しない)
 	testMissTodoRequest := TodoRequest{Title: "goの勉強", Description: "テストコードを書く", Status: "始めたばかり", Priority: 1}
-	assert.Error(t, testMissTodoRequest.TodoValidate(&gin.Context{}))
+	if ok := assert.Error(t, testMissTodoRequest.TodoValidate(&gin.Context{})); !ok {
+		t.Error("想定とは異なり、バリデーションエラーが発生しませんでした。")
+	}
 }
