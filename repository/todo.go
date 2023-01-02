a76d38607308fe474d062a47f960c6e96a14841d
arai0724 <arai0724yutaarai@gmail.com>
2023-01-02 19:56:12 +0900

    refactoring and add validation when user_id received in Request is not found in updateTodo function

--- repository/todo.go
+++ repository/todo.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"app/model"
-	"errors"
 
 	"gorm.io/gorm"
 )
@@ -29,7 +28,6 @@ func (r *todoRepository) FindTodo(id int64) (model.FindTodoResponse, error) {
 	var todo *model.Todo
 	err := r.db.First(&todo, id).Error
 	if err != nil {
-		handleNotFoundError(err)
 		return model.FindTodoResponse{}, err
 	}
 	return model.FindTodoResponse{Todo: todo}, nil
@@ -47,10 +45,9 @@ func (r *todoRepository) FindTodos() (model.FindTodosResponse, error) {
 func (r *todoRepository) CreateTodo(t *model.TodoRequest) (model.CreateTodoResponse, error) {
 	var user *model.User
 	if err := r.db.First(&user, t.UserID).Error; err != nil {
-		handleNotFoundError(err)
 		return model.CreateTodoResponse{}, err
 	}
-	todo := model.Todo{
+	todo := &model.Todo{
 		UserID:         t.UserID,
 		Title:          t.Title,
 		Description:    t.Description,
@@ -59,27 +56,30 @@ func (r *todoRepository) CreateTodo(t *model.TodoRequest) (model.CreateTodoRespo
 		ExpirationDate: t.ExpirationDate,
 	}
 
-	if err := r.db.Omit("created_at", "updated_at").Create(&todo).Error; err != nil {
+	if err := r.db.Omit("created_at", "updated_at").Save(todo).Error; err != nil {
 		return model.CreateTodoResponse{}, err
 	}
 	return model.CreateTodoResponse{ID: todo.ID}, nil
 }
 
 func (r *todoRepository) UpdateTodo(t *model.TodoRequest, id int64) (model.UpdateTodoResponse, error) {
+	var user *model.User
+	if err := r.db.First(&user, t.UserID).Error; err != nil {
+		return model.UpdateTodoResponse{}, err
+	}
 	var todo *model.Todo
 	err := r.db.First(&todo, id).Error
 	if err != nil {
-		handleNotFoundError(err)
 		return model.UpdateTodoResponse{}, err
 	}
-	updateTodo := model.Todo{
-		Title:          t.Title,
-		Description:    t.Description,
-		Status:         t.Status,
-		Priority:       t.Priority,
-		ExpirationDate: t.ExpirationDate,
-	}
-	if err := r.db.Omit("created_at", "updated_at").Model(&todo).Updates(updateTodo).Error; err != nil {
+
+	todo.Title = t.Title
+	todo.Description = t.Description
+	todo.Status = t.Status
+	todo.Priority = t.Priority
+	todo.ExpirationDate = t.ExpirationDate
+
+	if err := r.db.Omit("created_at", "updated_at").Save(todo).Error; err != nil {
 		return model.UpdateTodoResponse{}, err
 	}
 	return model.UpdateTodoResponse{ID: id}, nil
@@ -89,7 +89,6 @@ func (r *todoRepository) UpdateTodo(t *model.TodoRequest, id int64) (model.Updat
 func (r *todoRepository) DeleteTodo(t *model.Todo, id int64) (model.DeleteTodoResponse, error) {
 	err := r.db.First(&t, id).Error
 	if err != nil {
-		handleNotFoundError(err)
 		return model.DeleteTodoResponse{}, err
 	}
 
@@ -97,12 +96,4 @@ func (r *todoRepository) DeleteTodo(t *model.Todo, id int64) (model.DeleteTodoRe
 		return model.DeleteTodoResponse{}, err
 	}
 	return model.DeleteTodoResponse{}, nil
-}
-
-// handleNotFoundError is a function to name the error of gorm.ErrRecordNotFound for getting gormDB  out of Controller layer
-func handleNotFoundError(err error) error {
-	if errors.Is(err, gorm.ErrRecordNotFound) {
-		return errors.New("record not found")
-	}
-	return err
 }
