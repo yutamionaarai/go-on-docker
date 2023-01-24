9cfce40616a26564685aa801dcca486a0adf2948
arai0724 <arai0724yutaarai@gmail.com>
2023-01-25 04:22:03 +0900

    fix some indents

--- controller/todo_test.go
+++ controller/todo_test.go
@@ -39,7 +39,7 @@ func (s *TodoControllerTestSuite) TestFindTodo() {
 		wantStatusCode int
 	}{
 		"正常系データ": {
-			wantStatusCode: http.StatusOK,
+		    wantStatusCode: http.StatusOK,
  	    },
 	}
 	for name, tc := range testCases {
@@ -47,7 +47,7 @@ func (s *TodoControllerTestSuite) TestFindTodo() {
 		tc := tc
 		s.Run(name, func() {
 			s.T().Parallel()
-	        s.mock.On("FindTodo", int64(1)).Return(model.FindTodoResponse{Todo:testdata.Todo}, nil)
+            s.mock.On("FindTodo", int64(1)).Return(model.FindTodoResponse{Todo:testdata.Todo}, nil)
 			w := httptest.NewRecorder()
 			req, _ := http.NewRequest("GET", "/todos/1", nil)
 			s.router.ServeHTTP(w, req)
@@ -69,75 +69,74 @@ func (s *TodoControllerTestSuite) TestFindsTodo() {
 		name := name
 		tc := tc
 		s.Run(name, func() {
-			s.T().Parallel()
-			s.mock.On("FindTodos").Return(model.FindTodosResponse{Todos: testdata.Todos}, nil)
-			w := httptest.NewRecorder()
+            s.T().Parallel()
+            s.mock.On("FindTodos").Return(model.FindTodosResponse{Todos: testdata.Todos}, nil)
+            w := httptest.NewRecorder()
             req, _ := http.NewRequest("GET", "/todos/", nil)
             s.router.ServeHTTP(w, req)
-	        s.Equal(tc.wantStatusCode, w.Code)
+            s.Equal(tc.wantStatusCode, w.Code)
 		})
 	}
 }
 
 func (s *TodoControllerTestSuite) TestCreateTodo() {
-
 	s.T().Parallel()
 	testCases := map[string]struct {
-		todoRequest *model.TodoRequest
-		wantStatusCode int
-		wantTodoResponse model.CreateTodoResponse
+        todoRequest *model.TodoRequest
+        wantStatusCode int
+        wantTodoResponse model.CreateTodoResponse
 	}{
-		"正常系データ": {
-			todoRequest: testdata.TodoRequest,
-			wantStatusCode: http.StatusOK,
-			wantTodoResponse: model.CreateTodoResponse{ID:0},
-		},
-	}
+        "正常系データ": {
+            todoRequest: testdata.TodoRequest,
+            wantStatusCode: http.StatusOK,
+            wantTodoResponse: model.CreateTodoResponse{ID:0},
+        },
+    }
 	for name, tc := range testCases {
-		name := name
-		tc := tc
-		s.Run(name, func() {
-            s.T().Parallel()
+        name := name
+        tc := tc
+        s.Run(name, func() {
+			s.T().Parallel()
             s.mock.On("CreateTodo", tc.todoRequest).Return(tc.wantTodoResponse, nil)
             w := httptest.NewRecorder()
             jsonValue, _ := json.Marshal(tc.todoRequest)
             req, _ := http.NewRequest("POST", "/todos/", bytes.NewBuffer(jsonValue))
-		    s.router.ServeHTTP(w, req)
+            s.router.ServeHTTP(w, req)
             body, _ := ioutil.ReadAll(w.Result().Body)
-		    var response model.CreateTodoResponse
+            var response model.CreateTodoResponse
             json.Unmarshal(body, &response)
             s.Equal(tc.wantStatusCode, w.Code)
             s.Equal(tc.wantTodoResponse, response)
-		})
+        })
 	}
 }
 
 
 func (s *TodoControllerTestSuite) TestUpdateTodo() {
-	s.T().Parallel()
-	testCases := map[string]struct {
-		todoRequest *model.TodoRequest
-		wantStatusCode int
-		wantTodoResponse model.UpdateTodoResponse
-	}{
+    s.T().Parallel()
+    testCases := map[string]struct {
+        todoRequest *model.TodoRequest
+        wantStatusCode int
+        wantTodoResponse model.UpdateTodoResponse
+    }{
 		"正常系データ": {
-			todoRequest: testdata.TodoRequest,
-			wantStatusCode: http.StatusOK,
-			wantTodoResponse: model.UpdateTodoResponse{ID: 0},
-		},
-	}
-	for name, tc := range testCases {
-		name := name
-		tc := tc
-		s.Run(name, func() {
+            todoRequest: testdata.TodoRequest,
+            wantStatusCode: http.StatusOK,
+            wantTodoResponse: model.UpdateTodoResponse{ID: 0},
+        },
+    }
+    for name, tc := range testCases {
+        name := name
+        tc := tc
+        s.Run(name, func() {
 			s.T().Parallel()
-			w := httptest.NewRecorder()
-			s.mock.On("UpdateTodo", int64(1), tc.todoRequest).Return(tc.wantTodoResponse, nil)
-			jsonValue, _ := json.Marshal(tc.todoRequest)
+            w := httptest.NewRecorder()
+            s.mock.On("UpdateTodo", int64(1), tc.todoRequest).Return(tc.wantTodoResponse, nil)
+            jsonValue, _ := json.Marshal(tc.todoRequest)
             req, _ := http.NewRequest("PUT", "/todos/1", bytes.NewBuffer(jsonValue))
-			s.router.ServeHTTP(w, req)
-			body, _ := ioutil.ReadAll(w.Result().Body)
-		    var response model.UpdateTodoResponse
+            s.router.ServeHTTP(w, req)
+            body, _ := ioutil.ReadAll(w.Result().Body)
+            var response model.UpdateTodoResponse
             json.Unmarshal(body, &response)
             s.Equal(tc.wantStatusCode, w.Code)
             s.Equal(tc.wantTodoResponse, response)
@@ -148,24 +147,24 @@ func (s *TodoControllerTestSuite) TestUpdateTodo() {
 func (s *TodoControllerTestSuite) TestDeleteTodo() {
 	s.T().Parallel()
 	testCases := map[string]struct {
-		wantStatusCode int
+	    wantStatusCode int
 	}{
 		"正常系データ": {
-			wantStatusCode: http.StatusOK,
-		},
-	}
-	for name, tc := range testCases {
-		name := name
-		tc := tc
-		s.Run(name, func() {
-			s.T().Parallel()
-			w := httptest.NewRecorder()
-			s.mock.On("DeleteTodo", int64(1)).Return(model.DeleteTodoResponse{}, nil)
+            wantStatusCode: http.StatusOK,
+        },
+    }
+    for name, tc := range testCases {
+        name := name
+        tc := tc
+        s.Run(name, func() {
+            s.T().Parallel()
+            w := httptest.NewRecorder()
+            s.mock.On("DeleteTodo", int64(1)).Return(model.DeleteTodoResponse{}, nil)
             req, _ := http.NewRequest("DELETE", "/todos/1", nil)
             s.router.ServeHTTP(w, req)
-	        s.Equal(tc.wantStatusCode, w.Code)
-		})
-	}
+            s.Equal(tc.wantStatusCode, w.Code)
+        })
+    }
 }
 
 func TestTodoControllerTestSuite(t *testing.T) {
