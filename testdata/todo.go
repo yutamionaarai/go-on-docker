91e140bdc3fa3db5ae9ee4aef73652a47bedcdaf
arai0724 <arai0724yutaarai@gmail.com>
2023-01-29 19:17:39 +0900

    create test for abnormal pattern

--- testdata/todo.go
+++ testdata/todo.go
@@ -5,49 +5,53 @@ import (
 	"time"
 )
 
-
 var Todo = &model.Todo{
-	ID:          1,
-	Title:       "title",
-	Description: "Description",
-	Status:      "Status",
-	Priority:    1,
-	UserID:      1,
+	ID:             1,
+	Title:          "title",
+	Description:    "Description",
+	Status:         "Status",
+	Priority:       1,
+	UserID:         1,
 	ExpirationDate: &time.Time{},
-	CreatedAT: time.Now(),
-	UpdatedAt: time.Now(),
+	CreatedAT:      time.Now(),
+	UpdatedAt:      time.Now(),
 }
 
 var Todos = []*model.Todo{
-	&model.Todo {
-		ID: 1,
-		Title: "title",
-		Description: "Description",
-		Status: "Status",
-		Priority: 1,
-		UserID: 1,
+	&model.Todo{
+		ID:             1,
+		Title:          "title",
+		Description:    "Description",
+		Status:         "Status",
+		Priority:       1,
+		UserID:         1,
 		ExpirationDate: &time.Time{},
-		CreatedAT: time.Now(),
-		UpdatedAt: time.Now(),
+		CreatedAT:      time.Now(),
+		UpdatedAt:      time.Now(),
 	},
 	&model.Todo{
-		ID: 2,
-		Title: "title2",
-		Description: "Description2",
-		Status: "Status2",
-		Priority: 2,
-		UserID: 1,
+		ID:             2,
+		Title:          "title2",
+		Description:    "Description2",
+		Status:         "Status2",
+		Priority:       2,
+		UserID:         1,
 		ExpirationDate: &time.Time{},
-		CreatedAT: time.Now(),
-		UpdatedAt: time.Now(),
+		CreatedAT:      time.Now(),
+		UpdatedAt:      time.Now(),
 	},
 }
 
 var TodoRequest = &model.TodoRequest{
-	Title: "title",
-	Description: "Description",
-	Status: "Status",
-	Priority: 1,
+	Title:          "title",
+	Description:    "Description",
+	Status:         "Status",
+	Priority:       1,
 	ExpirationDate: &time.Time{},
-	UserID: 1,
+	UserID:         1,
+}
+
+var InvalidTodoRequest = &model.TodoRequest{
+	Description: "Description",
+	Priority:    1,
 }
